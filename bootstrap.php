e77758a2e056dfa8f042cfb1d1852a8d5ed79e4d
神马都要问 <shenmadouyaowen@gmail.com>
2016-11-22 14:20:41 +0800

    Add files via upload

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,10 @@
 <?php
-
-return function () {
-    echo 'Hello, world!';
+use Flarum\Event\UserWasRegistered;
+use Illuminate\Contracts\Events\Dispatcher;
+return function (Dispatcher $events) {
+    $events->listen(UserWasRegistered::class, function (UserWasRegistered $event) {
+        // do stuff before a post is saved
+        $event->user->is_activated=true;
+        $event->user->save();
+    });
 };
